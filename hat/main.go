1ed2e2cbfa686dfe97ab566f122bb88c8eae2df9
makki0205 <llxo2_5oxll@icloud.com>
2018-12-01 12:20:01 +0900

    .

--- hat/main.go
+++ hat/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/makki0205/config"
@@ -13,52 +13,50 @@ import (
 var mainUrl = config.Env("main_url")
 var SerialPort = config.Env("serial_port")
 
-type cap struct {
-	state bool
-}
-
-var Cap = cap{state: false}
-
 func main() {
-	err := SendIsWearing(false)
-	if err != nil {
-		log.Err(err)
-		panic(err)
-	}
+	SendIsWearing(false)
 	c := &serial.Config{Name: SerialPort, Baud: 9600}
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Err(err)
 		panic(err)
 	}
-	var buf = make([]byte, 5)
+	old := byte(0)
 	for {
+		var buf = make([]byte, 100)
 		_, err := s.Read(buf)
 		if err != nil {
 			log.Err(err)
 		}
-		if buf[0] == byte(0) {
-			if Cap.state == true {
-				SendIsWearing(false)
-			}
-		} else {
-			if Cap.state == false {
-				SendIsWearing(true)
-			}
+		if old != buf[0] {
+			//かぶっている
+			SendIsWearing(buf[0] == byte(49))
+			old = buf[0]
 		}
+		//	if buf[0] == byte(0) {
+		//		if Cap.state == true {
+		//			SendIsWearing(false)
+		//		}
+		//	} else {
+		//		if Cap.state == false {
+		//			SendIsWearing(true)
+		//		}
+		//	}
 	}
 }
 
-func SendIsWearing(flg bool) error {
-	Cap.state = flg
+func SendIsWearing(flg bool) {
+	fmt.Println("send!!", flg)
 	flgS := "false"
-
 	if flg {
 		flgS = "true"
 	}
-	_, err := http.Get(mainUrl + "/api/game/is_wearing/" + flgS)
-	if err != nil {
-		return errors.New("hat Driver [mainが見つかりません]")
-	}
-	return nil
+
+	go func() {
+		_, err := http.Get(mainUrl + "/api/game/is_wearing/" + flgS)
+		if err != nil {
+			log.Err(err)
+		}
+	}()
+
 }
